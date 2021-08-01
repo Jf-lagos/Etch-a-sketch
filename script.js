d2955664ad459a4b2812df656f1f60e2a6172beb
Jeff <lagosjf@gmail.com>
2021-08-01 15:33:46 +0200

    project finish

--- script.js
+++ script.js
@@ -1,56 +1,36 @@
-//Sets important constants and variables
-
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
-
-
-defaultGrid();
-//Creates a default grid sized 16x16 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-//Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    //Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-//Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            newCell.addEventListener('mouseover', e => e.target.classList.add('my-color-class'));
-            rows[j].appendChild(newCell).className = "cell";
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
         };
-
     };
+    $(".grid").width(660/x);
+    $(".grid").height(660/x);
 };
 
 
+function clearGrid(){
+    $(".grid").remove();
+};  
 
-const button = document.querySelector('button');
-button.addEventListener('click', clear)
+function refreshGrid(){
+    var z = prompt("How many boxes per side?");
+    clearGrid();
+    createGrid(z);
+};
 
-function clear() {
-  let select = document.querySelectorAll('#cell', '#gridRow');
-  select.forEach(function(toChange) {
-    toChange.style.color = "red";
-  })
-}
+$(document).ready(function() {
+    createGrid(16);
 
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
 
-/*
+    $(".newGrid").click(function() {
+        refreshGrid();
 
-- Select the classes "div", "cell"
-- Loop through them
-- Apply a new color
-
-*/
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
+});
